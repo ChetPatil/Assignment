b9df2cf01af1fa87270e4a48bb26b6b9b88f03b8
Chetan Patil <97183122+ChetPatil@users.noreply.github.com>
2022-11-03 21:48:18 +0530

    Add files via upload

---
+++ Assesment-1/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h2>About Us</h2>
+        <p>At zappos.com our purpose id simple: to live and deliver WOW.</p>
+        <p>Twenty years ago, we began as a small online retailer that only sild shoes. 
+          Today, we still sell shoes - as well as clothing, handbags, accessories and more.
+          That 'more' is providing the very best customer service, customer experience, and company culture.
+          We aim to inspire the world by showing it's possible to simultaneously deliver happiness to customers,
+          empleoyees, vendors, shareholders, and the community in a long term, sutanable way. 
+        </p>
+        <p>We hope that in future people won't even realize we started selling shoes online. Instead, 
+          They'll know Zappos as a service company that just happens to sell ____.
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Assesment-2/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import logo from './airbnb-logo.png';
+
+function App() {
+const hfjfh = 'https://www.citypng.com/photo/4406/hd-white-airbnb-logo-with-symbol-sign-icon-png-image' ;
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h2>The Evolution of Airbnb's Frontend</h2>
+        <p>@spikebrehm</p>
+          <img src={logo} className="App-logo" alt="logo"  />
+         </header>
+    </div>
+  );
+}
+
+export default App;
